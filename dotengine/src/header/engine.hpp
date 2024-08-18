604916dba04cd3f0949233ba528e84077f14b536
fsb3rke <berkeeavcii@gmail.com>
2024-08-18 23:53:51 +0300

    feat: big updates

--- dotengine/src/header/engine.hpp
+++ dotengine/src/header/engine.hpp
@@ -8,6 +8,8 @@
 #include "input.hpp"
 #include "engine_def.h"
 
+#define n(x) (x-1)
+
 class DotEngine {
 public:                     // {x, y}
     explicit DotEngine(std::vector<int> screenSize=DEFAULT_SCREEN_SIZE, std::string screenTitle=DEFAULT_SCREEN_TITLE, int spacing=DEFAULT_SPACING, char screenChar=DEFAULT_SCREEN_CHAR) {
@@ -28,7 +30,44 @@ public:                     // {x, y}
         
         this->renderWindow();
     }
-    
+
+    void renderWindow() {
+        system("cls");
+        for(long unsigned int i = 0; i < this->window.size(); ++i) {
+            for (long unsigned int j = 0; j < this->window.at(i).size(); ++j) {
+                std::cout << this->window.at(i).at(j) << utils::intToSpace(this->spacing);
+            }
+            std::cout << std::endl;
+        }
+    }
+
+    char getChar(int column, int pos) {
+        return this->window.at(column).at(pos);
+    }
+
+    char getChar(int pos) {
+        int cursor = 0;
+        int i, j;
+        bool isFound = false;
+
+        for (i = 0; i < this->screenSize[1] && !isFound; ++i) {
+            for (j = 0; j < this->screenSize[0] && !isFound; ++j) {
+                if (cursor == pos) {
+                    isFound = true;
+                } else
+                    cursor++;
+            }
+            if (!isFound)
+                cursor++;
+        }
+
+        return this->window.at(n(i)).at(n(j));
+    }
+
+    void changeChar(int column, int pos, char newChar) {
+        this->window.at(column).at(pos) = newChar;
+    }
+
 private:
     std::vector<int> screenSize;
     std::string screenTitle;
@@ -37,14 +76,7 @@ private:
     char screenChar;
     int spacing;
     
-    void renderWindow() {
-        for(int i = 0; i < this->window.size(); ++i) {
-            for (int j = 0; j < this->window.at(i).size(); ++j) {
-                std::cout << this->window.at(i).at(j) << utils::intToSpace(this->spacing);
-            }
-            std::cout << std::endl;
-        }
-    }
+
 
     // TODO: RENDER PAWNS;
 };
--- dotengine/src/main.cpp
+++ dotengine/src/main.cpp
@@ -1,7 +1,27 @@
 #include "header/engine.hpp"
 
 int main() {
-    DotEngine engine = *new DotEngine({15, 15}, "Berke", 1, '.');
+    DotEngine engine = *new DotEngine({8, 8}, "Berke", 1, '.');
     engine.initialize();
 
+    int columnSum = 0;
+    int posSum = 0;
+    int charPos = 'a';
+
+    while (engine.getChar(n(8), n(8)) == '.') {
+        engine.changeChar(columnSum, posSum, (char)charPos);
+        engine.renderWindow();
+
+        posSum++;
+        if (posSum > 7) {
+            columnSum++;
+            posSum = 0;
+        }
+        charPos++;
+    }
+
+    std::cout << engine.getChar(12) << std::endl;
+
+    // engine.changeChar(2, 5, 'A');
+    // engine.renderWindow();
 }
