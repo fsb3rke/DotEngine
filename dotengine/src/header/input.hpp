41585cc82ace6411eee8d332bda632f2bdcfdb35
fsb3rke <berkeeavcii@gmail.com>
2024-08-25 20:25:25 +0300

    feat: Added cross platform getch function for input system

--- dotengine/src/header/input.hpp
+++ dotengine/src/header/input.hpp
@@ -3,6 +3,10 @@
 
 #ifdef _WIN32
 #include <conio.h>
+#else
+#include <termios.h>
+#include <unistd.h>
+#include <stdio.h>
 #endif
 
 #define KEY_BACKSPACE 8
@@ -10,9 +14,25 @@
 #define KEY_ESC 27
 #define KEY_DEL 127
 
+inline int m_getch(void) {
+    #ifdef _WIN32
+    return getch();
+    #else
+    struct termios oldattr, newattr;
+    int ch;
+    tcgetattr(STDIN_FILENO, &oldattr);
+    newattr = oldattr;
+    newattr.c_lflag &= ~(ICANON | ECHO);
+    tcsetattr(STDIN_FILENO, TCSANOW, &newattr);
+    ch = getchar();
+    tcsetattr(STDIN_FILENO, TCSANOW, &oldattr);
+    return ch;
+    #endif
+}
+
 #ifdef _WIN32
 namespace Input {
-    auto getKey = [](int key){return getch()==key;};
+    auto getKey = [](int key){return m_getch()==key;};
 }
 #endif
 
